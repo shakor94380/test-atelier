e46afac8a5d3039b7de874fb894728d951fc6015
shakor94380 <shakor94380@gmail.com>
2023-04-12 10:45:15 +0200

    add html page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 const { port } = require('./config/index');
@@ -9,6 +10,10 @@ const routes = require('./api/routes/index');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use("/", routes);
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
 
 app.get("/health", (req, res) => {
   res.send("it work");
